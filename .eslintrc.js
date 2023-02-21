5282e53549ec607ba1885fed2e58431862233fe1
“luojinmin” <1121683285@qq.com>
2023-02-21 18:06:42 +0800

    feat: 公共数据

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,48 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-		es2021: true
-	},
-	parser: '@typescript-eslint/parser',
-	extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
-	plugins: ['prettier'],
-	parserOptions: {
-		ecmaVersion: 2020
-	},
-	rules: {
-		'@typescript-eslint/no-inferrable-types': 0,
-		'@typescript-eslint/no-var-requires': 0,
-		'no-var': 2,
-		eqeqeq: [
-			'error',
-			'always',
-			{
-				null: 'ignore'
-			}
-		],
-		'prettier/prettier': [
-			1,
-			{
-				endOfLine: 'auto',
-				// 使用单引号
-				singleQuote: true,
-				printWidth: 140, // 超过最大值换行
-				tabWidth: 2, // tab键宽度，默认为4
-				useTabs: true, // 使用tab（制表符）缩进而非空格
-				semi: false, // 行末是否加分号
-				trailingComma: 'none', // 最后一个对象元素加逗号
-				bracketSpacing: true // 对象，数组加空格
-			}
-		]
-	}
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    '@typescript-eslint/no-inferrable-types': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'no-var': 2,
+    eqeqeq: [
+      'error',
+      'always',
+      {
+        null: 'ignore',
+      },
+    ],
+    'prettier/prettier': [
+      1,
+      {
+        arrowParens: 'always',
+        bracketSameLine: true,
+        bracketSpacing: true,
+        embeddedLanguageFormatting: 'auto',
+        htmlWhitespaceSensitivity: 'css',
+        insertPragma: false,
+        jsxSingleQuote: false,
+        printWidth: 150,
+        proseWrap: 'never',
+        quoteProps: 'as-needed',
+        requirePragma: false,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        useTabs: false,
+        vueIndentScriptAndStyle: false,
+        singleAttributePerLine: false,
+      },
+    ],
+  },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,20 @@
 module.exports = {
-  endOfLine: 'auto',
-  // 使用单引号
-  singleQuote: true,
-  printWidth: 140, // 超过最大值换行
-  tabWidth: 2, // tab键宽度，默认为4
-  useTabs: true, // 使用tab（制表符）缩进而非空格
-  semi: false, // 行末是否加分号
-  trailingComma: 'none', // 最后一个对象元素加逗号
-  bracketSpacing: true // 对象，数组加空格
-};
+    arrowParens: 'always',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    htmlWhitespaceSensitivity: 'css',
+    insertPragma: false,
+    jsxSingleQuote: false,
+    printWidth: 150,
+    proseWrap: 'never',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+    useTabs: false,
+    vueIndentScriptAndStyle: false,
+    singleAttributePerLine: false
+}
