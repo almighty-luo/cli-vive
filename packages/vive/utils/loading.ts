c366ebe37498ff64826d8aba0f3af443ddf8ef01
“luojinmin” <1121683285@qq.com>
2023-03-07 18:19:40 +0800

    feat: loading

--- packages/vive/utils/loading.ts
+++ packages/vive/utils/loading.ts
@@ -3,19 +3,24 @@ import { Color } from "ora"
 class Loading {
 	public ora
 	public title
+	public spinner
 	private _colorYellow = "yellow"
 	private _colorBule = "bule"
 	constructor(title: string) {
 		this.ora = ora
 		this.title = title
 	}
-
-	public start(title: string) {
-		const spinner = this.ora(title).start()
+	/* 开启 */
+	public start(title: string): void {
+		this.spinner = this.ora(title).start()
 		const type = false
 		setTimeout(() => {
-			spinner.color = (type ? this._colorBule : this._colorYellow) as Color
+			this.spinner.color = (type ? this._colorBule : this._colorYellow) as Color
 		}, 500)
+	}
+	/* 关闭 */
+	public close(): void {
+		this.spinner.stop()
 	}
 }
 
